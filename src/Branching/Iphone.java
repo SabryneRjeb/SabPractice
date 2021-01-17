57d08721068143e09b3f1b118a63d7da48f79646
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-17 15:06:00 -0500

    Committing more work

--- src/Branching/Iphone.java
+++ src/Branching/Iphone.java
@@ -5,6 +5,9 @@ public class Iphone {
         Phone Iphone = new Phone("Apple","Iphone","Silver",2021,1200);
         System.out.println(Iphone);
 
+        System.out.println("Hello world");
+        System.out.println("More work");
+
 
     }
 }
