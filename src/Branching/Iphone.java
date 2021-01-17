2f70bbee6ec39f1b432ebbbc06fb47926859fa49
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-17 15:04:53 -0500

    Committing

--- src/Branching/Iphone.java
+++ src/Branching/Iphone.java
@@ -3,5 +3,8 @@ package Branching;
 public class Iphone {
     public static void main(String[] args) {
         Phone Iphone = new Phone("Apple","Iphone","Silver",2021,1200);
+        System.out.println(Iphone);
+
+
     }
 }
